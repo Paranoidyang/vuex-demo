af7b48d1ff71de26583bc25dd1e0121bfdf5a275
paranoidyang <paranoidyang@163.com>
2017-07-11 19:01:30 +0800

    vuex入门示例

---
+++ src/components/count.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h2>{{msg}}</h2>
+        <hr/>
+        <h3>{{count}}</h3>
+        <div>
+            <button @click="$store.commit('add')">+</button>
+            <button @click="$store.commit('reduce')">-</button>
+        </div>
+    </div>
+</template>
+<script>
+    import {mapState} from 'vuex';
+    import store from '@/vuex/store'
+    export default{
+        data(){
+            return{
+                msg:'Hello Vuex',
+ 
+            }
+        },
+        store,
+        //方法一
+        // computed:{
+        //     count(){
+        //         return this.$store.state.count;
+        //     }
+        // },
+        
+        //方法二
+        // computed:mapState({
+        //         count:state=>state.count
+        //  })
+        
+        //方法三
+         computed:mapState(["count"])
+    }
+</script>
